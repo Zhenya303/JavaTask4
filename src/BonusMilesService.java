5c6928982af73967de23f93035637ee0da472d52
“Zhenya” <dyadicheve@mail.ru>
2023-04-09 22:10:40 +0300

    mainChanged

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,7 +1,6 @@
 public class BonusMilesService {
 
-    public int calculate (int a){
-        int result = a / 20;
-        return result;
+    public int calculate (int price){
+        return price / 20;
     }
 }
